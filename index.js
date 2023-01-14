c3642f404ea97ea54f27c19ec8152815e0d1b4cf
chepyego <jchepyego@gmail.com>
2023-01-15 01:00:45 +0300

    add javascript

---
+++ index.js
@@ -0,0 +1,29 @@
+const projectOne = document.getElementById("project-1");
+const projectTwo = document.getElementById("project-2");
+const projectName = document.getElementById("name");
+
+// projectTwo.remove();
+// alert("are you sure you want to delete this");
+projectName.addEventListener("mouseover",formResponsive );
+projectName.addEventListener("mouseleave",formLeave );
+
+// function formLeave(){
+//     projectName.style.left = -75
+//     projectName.style.right = - 75
+// }
+
+
+
+    
+function editProject(){
+    projectOne.addEventListener("click",formResponsive);
+}
+
+function formResponsive(){
+    // projectOne.style.backgroundColor = "red"
+    projectOne.classList.add("project-hover");
+    // projectOne.remove();
+
+}
+formResponsive();
+
