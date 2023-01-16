2aea50a995bd8551ac753bc764fdf3389cbc2e82
chepyego <jchepyego@gmail.com>
2023-01-16 12:41:36 +0300

    add about page

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const projectOne = document.getElementById("project-1");
 const projectTwo = document.getElementById("project-2");
 const projectName = document.getElementById("name");
 
+const homeMenu = document.querySelector(".name");
+
 // projectTwo.remove();
 // alert("are you sure you want to delete this");
 projectName.addEventListener("mouseover",formResponsive );
-projectName.addEventListener("mouseleave",formLeave );
+// projectName.addEventListener("mouseleave",formLeave );
 
 // function formLeave(){
 //     projectName.style.left = -75
@@ -17,11 +19,14 @@ projectName.addEventListener("mouseleave",formLeave );
     
 function editProject(){
     projectOne.addEventListener("click",formResponsive);
+    homeMenu.addEventListener("click", formResponsive);
 }
 
 function formResponsive(){
     // projectOne.style.backgroundColor = "red"
     projectOne.classList.add("project-hover");
+    homeMenu.cr
+    homeMenu.classList.add("mobile-navigation")
     // projectOne.remove();
 
 }
